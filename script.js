083945d498ba44665d78e659fece0f91430bc57b
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-06-20 18:31:54 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var menu = document.getElementById("menubar");
+var menu2 = document.getElementById("menubar2");
+
+menu.onclick = function () {
+  menu.classList.toggle("openmenu");
+}
+menu2.onclick = function () {
+  menu2.classList.toggle("openmenu2");
+}
